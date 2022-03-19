6323cae4ee0d7ec56a4a567d6b1114aac088d987
Dhandapani1408 <dhandapani1408@gmail.com>
2022-03-19 13:50:31 +0530

    Updated Class file name and testng.xml

--- src/test/java/testcases/TC_0003.java
+++ src/test/java/testcases/TC_0003.java
@@ -1,32 +1,33 @@
 package testcases;
 
-
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import com.pages.HomePage;
 import com.pages.LoginPage;
 import com.testNG.base.Annotations;
 
-public class TC_0002_Test extends Annotations{
+public class TC_0003 extends Annotations{
 
 	@BeforeTest
-	public void beforeTest() {
-		testcaseName = "TC002_CreateLead";
-		testcaseDec = "Create Lead and Validate wheather lead is created";
+	public void setData() {
+		testcaseName = "TC003_FindLead";
+		testcaseDec = "Login into leaftapsa and Find Leads";
 		author = "dhandapani";
 		category = "sanity";
 		excelFileName = "TC001";
-		sheetName = "Create";
-	}
-	
+		sheetName = "Find";
+	} 
+
 	@Test(dataProvider="fetchData") 
-	public void createLead(String uName, String pwd,String compName,String fName,String lName) {
+	public void loginAndFind(String uName, String pwd,String compName,String fName,String lName) {
 		new LoginPage()
 		.enterUserName(uName)
 		.enterPassWord(pwd) 
 		.clickLogin().clickCRMSFA().clickLeadsMenu().clickCreateLeads()
 		.enterCompanyName(compName).enterFirstName(fName).enterLastName(lName).clickSubmitForCreateLead()
-		.validateCreatedCompanyName().validateCreatedFirstName().validateCreatedLastName();
+		.validateCreatedCompanyName().validateCreatedFirstName().validateCreatedLastName()
+		.clickFindLeads().findLeadsByNameAndId().validateFirstRowResults();
 		
 	}
 
